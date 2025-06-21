428d81f191dffb7af7b6df30104b675a52282eb4
VED2380 <91709231+VED2380@users.noreply.github.com>
2025-06-21 14:42:06 +0530

    Update models.py

--- src/utils/models.py
+++ src/utils/models.py
@@ -1,1 +1,61 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, BatchNormalization, Dense, Dropout, GlobalAveragePooling2D, Add
+from tensorflow.keras.models import Model
 
+def focal_loss(gamma=2.0, alpha=0.25):
+    def focal_loss_fn(y_true, y_pred):
+        y_true = tf.cast(y_true, tf.float32)
+        y_pred = tf.clip_by_value(y_pred, tf.keras.backend.epsilon(), 1.0 - tf.keras.backend.epsilon())
+        cross_entropy = -y_true * tf.math.log(y_pred)
+        weight = tf.pow(1.0 - y_pred, gamma) * y_true
+        return tf.reduce_mean(alpha * weight * cross_entropy)
+    return focal_loss_fn
+
+def resnet_block(x, filters, kernel_size=3, stride=1):
+    y = Conv2D(filters, kernel_size, strides=stride, padding='same')(x)
+    y = BatchNormalization()(y)
+    y = tf.keras.activations.relu(y)
+    y = Conv2D(filters, kernel_size, padding='same')(y)
+    y = BatchNormalization()(y)
+    if stride > 1 or x.shape[-1] != filters:
+        x = Conv2D(filters, 1, strides=stride, padding='same')(x)
+    return tf.keras.activations.relu(Add()([x, y]))
+
+def build_resnet18(input_shape=(75, 50, 1), num_classes=4):
+    inputs = Input(shape=input_shape)
+    x = Conv2D(64, 7, strides=2, padding='same')(inputs)
+    x = BatchNormalization()(x)
+    x = tf.keras.activations.relu(x)
+    x = MaxPooling2D(3, strides=2, padding='same')(x)
+    
+    x = resnet_block(x, 64)
+    x = resnet_block(x, 64)
+    x = resnet_block(x, 128, stride=2)
+    x = resnet_block(x, 128)
+    x = resnet_block(x, 256, stride=2)
+    x = resnet_block(x, 256)
+    x = resnet_block(x, 512, stride=2)
+    x = resnet_block(x, 512)
+    
+    x = GlobalAveragePooling2D()(x)
+    x = Dense(512, activation='relu')(x)
+    x = Dropout(0.5)(x)
+    outputs = Dense(num_classes, activation='softmax')(x)
+    
+    return Model(inputs, outputs)
+
+def build_resnet50(input_shape=(75, 50, 3), num_classes=4):
+    base_model = tf.keras.applications.ResNet50(weights='imagenet', include_top=False, input_shape=input_shape)
+    for layer in base_model.layers[:-40]:
+        layer.trainable = False
+    for layer in base_model.layers[-40:]:
+        layer.trainable = True
+
+    inputs = Input(shape=input_shape)
+    x = base_model(inputs, training=True)
+    x = GlobalAveragePooling2D()(x)
+    x = Dense(512, activation='relu')(x)
+    x = Dropout(0.5)(x)
+    outputs = Dense(num_classes, activation='softmax')(x)
+    
+    return Model(inputs, outputs)
